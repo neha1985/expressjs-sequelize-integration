e72581db88637534b4484ca339c769a7ea918945
Neha Gupta <neha.gupta1985@gmail.com>
2017-01-29 19:46:45 +0530

    removed unused files

--- controllers/main.js
+++
@@ -1,8 +0,0 @@
-const Users = require('../db').Users;
-
-module.exports = {
-    getHomePage: (req, res) => {
-        res.status(200);
-        res.render("index", { title: "EXPRESS-SEQUELIZE" });
-    }
-};
